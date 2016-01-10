384d5c6800b114079e5a518ddb318527443377d4
Gosstapher <gosstapher@gmail.com>
2016-01-10 18:21:42 +0000

    bookings now available in nav bar

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,40 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    Booking.create(booking_params)
+    redirect_to(bookings_path)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to(bookings_path)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to(booking_path)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :gig_id, :gigs, :users)
+  end
+end
