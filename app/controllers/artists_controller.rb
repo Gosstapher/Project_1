6e41a7e1c5b9dffd637bf56d95c25ec41923ea75
Gosstapher <gosstapher@gmail.com>
2016-01-18 09:13:07 +0000

    presentation given, expanded functions from here

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource except: [:create]
   before_action :authenticate_user!
   def index
     @artists = Artist.all
